ba02bab54366ef4a90eb9405e4d64a9e33c3fd82
Konstantin Ryabitsev <konstantin@linuxfoundation.org>
2020-12-21 13:48:51 -0500

    Add -f to "b4 mbox" to filter dupes
    When saving to a maildir, add option to filter out dupes. Note, that
    this requires going through the entire maildir to collect message-ids,
    so it's not going to be a great experience on large maildirs.
    
    Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

--- b4/command.py
+++ b4/command.py
@@ -84,6 +84,8 @@ def cmd():
     # b4 mbox
     sp_mbox = subparsers.add_parser('mbox', help='Download a thread as an mbox file')
     cmd_mbox_common_opts(sp_mbox)
+    sp_mbox.add_argument('-f', '--filter-dupes', dest='filterdupes', action='store_true', default=False,
+                         help='When adding messages to existing maildir, filter out duplicates')
     sp_mbox.set_defaults(func=cmd_mbox)
 
     # b4 am
