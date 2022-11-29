d377f8ec13eac3b0ae253c89477b56b0d9cec42f
Konstantin Ryabitsev <konstantin@linuxfoundation.org>
2022-11-29 12:11:39 -0500

    tests: update tests for ez/preserve-tripledash
    Test that we properly preserve --- info in commits.
    
    Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

--- tests/test_ez.py
+++ tests/test_ez.py
@@ -8,6 +8,7 @@ import b4.command
 
 @pytest.fixture(scope="function")
 def prepdir(gitdir):
+    b4.MAIN_CONFIG.update({'prep-cover-strategy': 'branch-description'})
     parser = b4.command.setup_parser()
     b4args = ['--no-stdin', '--no-interactive', '--offline-mode', 'prep', '-n', 'pytest']
     cmdargs = parser.parse_args(b4args)
@@ -15,43 +16,56 @@ def prepdir(gitdir):
     yield gitdir
 
 
-@pytest.mark.parametrize('mboxf, rep, trargs, compareargs, compareout, b4cfg', [
-    ('trailers-thread-with-followups', None, [],
+@pytest.mark.parametrize('mboxf, bundlef, rep, trargs, compareargs, compareout, b4cfg', [
+    ('trailers-thread-with-followups', None, None, [],
      ['log', '--format=%ae%n%s%n%b---', 'HEAD~4..'], 'trailers-thread-with-followups',
      {'shazam-am-flags': '--signoff'}),
-    ('trailers-thread-with-cover-followup', None, [],
+    ('trailers-thread-with-cover-followup', None, None, [],
      ['log', '--format=%ae%n%s%n%b---', 'HEAD~4..'], 'trailers-thread-with-cover-followup',
      {'shazam-am-flags': '--signoff'}),
     # Test matching trailer updates by subject when patch-id changes
-    ('trailers-thread-with-followups', (b'vivendum', b'addendum'), [],
+    ('trailers-thread-with-followups', None, (b'vivendum', b'addendum'), [],
      ['log', '--format=%ae%n%s%n%b---', 'HEAD~4..'], 'trailers-thread-with-followups',
      {'shazam-am-flags': '--signoff'}),
+    # Test that we properly perserve commits with --- in them
+    ('trailers-thread-with-followups', 'trailers-with-tripledash', None, [],
+     ['log', '--format=%ae%n%s%n%b---', 'HEAD~4..'], 'trailers-thread-with-followups-and-tripledash',
+     None),
 ])
-def test_trailers(sampledir, prepdir, mboxf, rep, trargs, compareargs, compareout, b4cfg):
-    b4.MAIN_CONFIG.update(b4cfg)
+def test_trailers(sampledir, prepdir, mboxf, bundlef, rep, trargs, compareargs, compareout, b4cfg):
+    if b4cfg:
+        b4.MAIN_CONFIG.update(b4cfg)
     config = b4.get_main_config()
-    assert config.get('shazam-am-flags') == '--signoff'
     mfile = os.path.join(sampledir, f'{mboxf}.mbox')
-    cfile = os.path.join(sampledir, f'{compareout}.verify')
     assert os.path.exists(mfile)
-    assert os.path.exists(cfile)
-    parser = b4.command.setup_parser()
-    if rep:
-        with open(mfile, 'rb') as fh:
-            contents = fh.read()
-        contents = contents.replace(rep[0], rep[1])
-        tfile = os.path.join(prepdir, '.git', 'modified.mbox')
-        with open(tfile, 'wb') as fh:
-            fh.write(contents)
+    if bundlef:
+        bfile = os.path.join(sampledir, f'{bundlef}.bundle')
+        assert os.path.exists(bfile)
+        gitargs = ['pull', '--rebase', bfile]
+        out, logstr = b4.git_run_command(None, gitargs)
+        assert out == 0
     else:
-        tfile = mfile
-    b4args = ['--no-stdin', '--no-interactive', '--offline-mode', 'shazam', '--no-add-trailers', '-m', tfile]
+        assert config.get('shazam-am-flags') == '--signoff'
+        if rep:
+            with open(mfile, 'rb') as fh:
+                contents = fh.read()
+            contents = contents.replace(rep[0], rep[1])
+            tfile = os.path.join(prepdir, '.git', 'modified.mbox')
+            with open(tfile, 'wb') as fh:
+                fh.write(contents)
+        else:
+            tfile = mfile
+        b4args = ['--no-stdin', '--no-interactive', '--offline-mode', 'shazam', '--no-add-trailers', '-m', tfile]
+        parser = b4.command.setup_parser()
 
-    cmdargs = parser.parse_args(b4args)
-    with pytest.raises(SystemExit) as e:
-        b4.mbox.main(cmdargs)
-        assert e.type == SystemExit
-        assert e.value.code == 0
+        cmdargs = parser.parse_args(b4args)
+        with pytest.raises(SystemExit) as e:
+            b4.mbox.main(cmdargs)
+            assert e.type == SystemExit
+            assert e.value.code == 0
+
+    cfile = os.path.join(sampledir, f'{compareout}.verify')
+    assert os.path.exists(cfile)
 
     parser = b4.command.setup_parser()
     b4args = ['--no-stdin', '--no-interactive', '--offline-mode', 'trailers', '--update', '-m', mfile] + trargs
