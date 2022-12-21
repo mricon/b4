fc81d901a96a1df9a198c840c9f1a7d63ef1a720
Konstantin Ryabitsev <konstantin@linuxfoundation.org>
2022-12-21 09:38:47 -0500

    Prepare for 0.11.1
    Set version to 0.11.1 in preparation for the release.
    
    Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

--- b4/__init__.py
+++ b4/__init__.py
@@ -53,7 +53,7 @@ except ModuleNotFoundError:
 # global setting allowing us to turn off networking
 can_network = True
 
-__VERSION__ = '0.11.1-dev'
+__VERSION__ = '0.11.1'
 PW_REST_API_VERSION = '1.2'
 
 
