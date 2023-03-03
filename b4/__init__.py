47b8907b9127101d89aa37ad13ba1b9444f59c13
Konstantin Ryabitsev <konstantin@linuxfoundation.org>
2023-03-03 15:40:33 -0500

    init: up version to 0.12.2-dev
    Prepare for a round of backports.
    
    Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

--- b4/__init__.py
+++ b4/__init__.py
@@ -59,7 +59,7 @@ except ModuleNotFoundError:
 # global setting allowing us to turn off networking
 can_network = True
 
-__VERSION__ = '0.12.1'
+__VERSION__ = '0.12.2-dev'
 PW_REST_API_VERSION = '1.2'
 
 
