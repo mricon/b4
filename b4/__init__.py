mricon/b4

commit 8e59593f24e24f7ba6bb0de0fa498b0e32b946ad

Add linktrailermask config variable
This allows overriding the behaviour of the --add-link switch to "git
am/shazam". For example, projects that require Message-ID trailers
instead of Link: trailers can have it configured as:

b4.linktrailermask = Message-ID: <%s>

Suggested-by: Palmer Dabbelt <palmer@rivosinc.com>
Link: https://lore.kernel.org/tools/20230216164841.3356-1-palmer@rivosinc.com/
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

b4/__init__.py
@@ -94,6 +94,10 @@ DEFAULT_CONFIG = {
     'midmask': LOREADDR + '/all/%s',
     'linkmask': LOREADDR + '/r/%s',
     'searchmask': LOREADDR + '/all/?x=m&t=1&q=%s',
+    # You can override the format for the Link: trailer, e.g.
+    # if you would rather use the Message-Id trailer. It takes the
+    # message-id as the expansion for %s
+    # linktrailermask = Message-Id: <%s>
     'listid-preference': '*.feeds.kernel.org,*.linux.dev,*.kernel.org,*',
     'save-maildirs': 'no',
     # off: do not bother checking attestation
@@ -519,7 +523,8 @@ class LoreSeries:
             self.add_extra_trailers(self.patches[0].followup_trailers)  # noqa
 
     def get_am_ready(self, noaddtrailers=False, covertrailers=False, addmysob=False, addlink=False,
-                     linkmask=None, cherrypick=None, copyccs=False, allowbadchars=False) -> List[email.message.Message]:
+                     linktrailer=None, cherrypick=None, copyccs=False,
+                     allowbadchars=False) -> List[email.message.Message]:
 
         usercfg = get_user_config()
         config = get_main_config()
@@ -579,11 +584,21 @@ class LoreSeries:
             if lmsg is not None:
                 extras = list()
                 if addlink:
-                    if linkmask is None:
-                        linkmask = config.get('linkmask')
-                    linkval = linkmask % lmsg.msgid
-                    lltr = LoreTrailer(name='Link', value=linkval)
-                    extras.append(lltr)
+                    if linktrailer is None:
+                        ltrmask = config.get('linktrailermask')
+                        if ltrmask:
+                            if ltrmask.find(':'):
+                                lparts = ltrmask.split(':', maxsplit=1)
+                                llname = lparts[0].strip()
+                                llval = lparts[1].strip() % lmsg.msgid
+                                linktrailer = LoreTrailer(name=llname, value=llval)
+                            else:
+                                logger.critical('linktrailermask does not look like a valid trailer')
+                        else:
+                            llval = config.get('linkmask', LOREADDR + '/r/%s') % lmsg.msgid
+                            linktrailer = LoreTrailer(name='Link', value=llval)
+                    if linktrailer:
+                        extras.append(linktrailer)
 
                 if attsame and not attcrit:
                     if attmark:

tests/test___init__.py
@@ -87,6 +87,7 @@ def test_parse_trailers(sampledir, source, expected):
     ('single', {}, {'addmysob': True}, 'addmysob', {}),
     ('single', {}, {'addmysob': True, 'copyccs': True}, 'copyccs', {}),
     ('single', {}, {'addmysob': True, 'addlink': True}, 'addlink', {}),
+    ('single', {}, {'addmysob': True, 'addlink': True}, 'addmsgid', {'linktrailermask': 'Message-Id: <%s>'}),
     ('single', {}, {'addmysob': True, 'copyccs': True}, 'ordered',
      {'trailer-order': 'Cc,Tested*,Reviewed*,*'}),
     ('single', {'sloppytrailers': True}, {'addmysob': True}, 'sloppy', {}),

b4/mbox.py
@@ -99,7 +99,7 @@ def make_am(msgs: List[email.message.Message], cmdargs: argparse.Namespace, msgi
     try:
         am_msgs = lser.get_am_ready(noaddtrailers=cmdargs.noaddtrailers,
                                     covertrailers=covertrailers, addmysob=cmdargs.addmysob,
-                                    addlink=cmdargs.addlink, linkmask=config['linkmask'], cherrypick=cherrypick,
+                                    addlink=cmdargs.addlink, cherrypick=cherrypick,
                                     copyccs=cmdargs.copyccs, allowbadchars=cmdargs.allowbadchars)
     except KeyError:
         sys.exit(1)
@@ -351,6 +351,7 @@ def make_am(msgs: List[email.message.Message], cmdargs: argparse.Namespace, msgi
                 'authorname': cmsg.fromname,
                 'authoremail': cmsg.fromemail,
                 'covermessage': covermessage,
+                'mid': top_msgid,
                 'midurl': linkurl,
             }
             if len(am_msgs) > 1:

commit 00303592f25693cb4eddf58fb51063afcff8dceb

Fix incorrect message-id used for Link: trailers
When I added ability to modify the Link: trailer itself, I broke the
whole thing so the same message-id was repeated for all patches in the
series. This fixes the problem and adds the test to make sure we catch
it in the future.

Reported-by: Michael Ellerman <mpe@kernel.org>
Fixes: 8e59593f24e2 ("Add linktrailermask config variable")
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

b4/__init__.py
@@ -526,8 +526,7 @@ class LoreSeries:
             self.add_extra_trailers(self.patches[0].followup_trailers)  # noqa
 
     def get_am_ready(self, noaddtrailers=False, covertrailers=False, addmysob=False, addlink=False,
-                     linktrailer=None, cherrypick=None, copyccs=False,
-                     allowbadchars=False) -> List[email.message.Message]:
+                     cherrypick=None, copyccs=False, allowbadchars=False) -> List[email.message.Message]:
 
         usercfg = get_user_config()
         config = get_main_config()
@@ -587,21 +586,21 @@ class LoreSeries:
             if lmsg is not None:
                 extras = list()
                 if addlink:
-                    if linktrailer is None:
-                        ltrmask = config.get('linktrailermask')
-                        if ltrmask:
-                            if ltrmask.find(':'):
-                                lparts = ltrmask.split(':', maxsplit=1)
-                                llname = lparts[0].strip()
-                                llval = lparts[1].strip() % lmsg.msgid
-                                linktrailer = LoreTrailer(name=llname, value=llval)
-                            else:
-                                logger.critical('linktrailermask does not look like a valid trailer')
+                    linktrailer = None
+                    ltrmask = config.get('linktrailermask')
+                    if ltrmask:
+                        if ltrmask.find(':'):
+                            lparts = ltrmask.split(':', maxsplit=1)
+                            llname = lparts[0].strip()
+                            llval = lparts[1].strip() % lmsg.msgid
+                            linktrailer = LoreTrailer(name=llname, value=llval)
                         else:
-                            llval = config.get('linkmask', LOREADDR + '/r/%s') % lmsg.msgid
-                            linktrailer = LoreTrailer(name='Link', value=llval)
-                    if linktrailer:
-                        extras.append(linktrailer)
+                            logger.critical('linktrailermask does not look like a valid trailer, using defaults')
+
+                    if not linktrailer:
+                        llval = config.get('linkmask', LOREADDR + '/r/%s') % lmsg.msgid
+                        linktrailer = LoreTrailer(name='Link', value=llval)
+                    extras.append(linktrailer)
 
                 if attsame and not attcrit:
                     if attmark:

tests/test___init__.py
@@ -93,6 +93,7 @@ def test_parse_trailers(sampledir, source, expected):
     ('single', {'sloppytrailers': True}, {'addmysob': True}, 'sloppy', {}),
     ('with-cover', {}, {'addmysob': True}, 'defaults', {}),
     ('with-cover', {}, {'covertrailers': True, 'addmysob': True}, 'covertrailers', {}),
+    ('with-cover', {}, {'addmysob': True, 'addlink': True}, 'addlink', {}),
     ('custody', {}, {'addmysob': True, 'copyccs': True}, 'unordered', {}),
     ('custody', {}, {'addmysob': True, 'copyccs': True}, 'ordered',
      {'trailer-order': 'Cc,Fixes*,Link*,Suggested*,Reviewed*,Tested*,*'}),