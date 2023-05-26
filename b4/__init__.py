mricon/b4

commit 7d1a05ed58f244640ab59680ac4b836d7ca726c8

trailers: ignore non-recognized link trailers
If a link contains a '@', we wrongly consider it a person-trailer.
Ignore any URL-containing trailers that we don't explicitly recognize.

Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

b4/__init__.py
@@ -1540,6 +1540,9 @@ class LoreMessage:
                     if not mperson and lname not in nonperson:
                         logger.debug('Ignoring %s (not a recognized non-person trailer)', line)
                         continue
+                    if re.search(r'https?://', ovalue):
+                        logger.debug('Ignoring a non-recognized link trailer')
+                        continue
 
                 extinfo = None
                 mextinfo = re.search(r'(.*\S+)(\s+#[^#]+)$', ovalue)

commit 0a6be433883dee544b991e218e0f505dba356a88

trailers: accept recognized link trailers
It looks like all all link trailers were ignored.

Now we accept 'link' and 'buglink' that were already in the list of non
person categories.

Note that existing test files have been modified. You will probably need
to run "pytest --cache-clear" to avoid using the previous version.

Fixes: 7d1a05e ("trailers: ignore non-recognized link trailers")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
Link: https://msgid.link/20230525-closes-tags-v1-1-ed41b1773cb6@tessares.net
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

b4/__init__.py
@@ -1656,7 +1656,8 @@ class LoreMessage:
     def find_trailers(body: str, followup: bool = False) -> Tuple[List[LoreTrailer], List[str]]:
         ignores = {'phone', 'email'}
         headers = {'subject', 'date', 'from'}
-        nonperson = {'fixes', 'subject', 'date', 'link', 'buglink', 'obsoleted-by', 'change-id', 'base-commit'}
+        links = {'link', 'buglink'}
+        nonperson = links | {'fixes', 'subject', 'date', 'obsoleted-by', 'change-id', 'base-commit'}
         # Ignore everything below standard email signature marker
         body = body.split('\n-- \n', 1)[0].strip() + '\n'
         # Fix some more common copypasta trailer wrapping
@@ -1695,7 +1696,8 @@ class LoreMessage:
                     if not mperson and lname not in nonperson:
                         logger.debug('Ignoring %s (not a recognized non-person trailer)', line)
                         continue
-                    if re.search(r'https?://', ovalue):
+                    mlink = re.search(r'https?://', ovalue)
+                    if mlink and lname not in links:
                         logger.debug('Ignoring %s (not a recognized link trailer)', line)
                         continue
 

tests/samples/trailers-thread-with-cover-followup.mbox
@@ -225,6 +225,7 @@ To: list@example.org
 > Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>
 
 Tested-by: Follow Upper <follow.upper@example.org>
+Link: https://example.org
 
 -- 
 Follow Upper

tests/samples/trailers-thread-with-cover-followup.verify
@@ -4,6 +4,7 @@ Life imitatus artem.
 
 Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>
 Tested-by: Follow Upper <follow.upper@example.org>
+Link: https://example.org
 Reviewed-by: Cover Upper <cover.upper@example.org>
 Signed-off-by: Test Override <test-override@example.com>
 ---