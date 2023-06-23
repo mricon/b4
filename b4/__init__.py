cea8d19a7041269a34f76bb8b99129ceb30f6945
Konstantin Ryabitsev <konstantin@linuxfoundation.org>
2023-06-23 13:50:24 -0400

    Up release to 0.12.3
    Get ready to release 0.12.3.
    
    Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

--- b4/__init__.py
+++ b4/__init__.py
@@ -60,7 +60,7 @@ except ModuleNotFoundError:
 # global setting allowing us to turn off networking
 can_network = True
 
-__VERSION__ = '0.12.3-dev'
+__VERSION__ = '0.12.3'
 PW_REST_API_VERSION = '1.2'
 
 
