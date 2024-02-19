d1571b04cf591bd63a2aed302e47dc259d7b26a6
Konstantin Ryabitsev <konstantin@linuxfoundation.org>
2024-02-19 15:16:39 -0500

    Fix PYTHONPATH to include src/
    Make sure we're using the src subdir of the repository.
    
    Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

--- b4.sh
+++ b4.sh
@@ -6,5 +6,5 @@
 REAL_SCRIPT=$(realpath -e ${BASH_SOURCE[0]})
 SCRIPT_TOP="${SCRIPT_TOP:-$(dirname ${REAL_SCRIPT})}"
 
-PYTHONPATH="${SCRIPT_TOP}:${SCRIPT_TOP}/patatt${PYTHONPATH:+:$PYTHONPATH}" \
+PYTHONPATH="${SCRIPT_TOP}/src:${SCRIPT_TOP}/patatt${PYTHONPATH:+:$PYTHONPATH}" \
 	exec python3 "${SCRIPT_TOP}/src/b4/command.py" "${@}"
