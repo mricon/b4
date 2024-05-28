mricon/b4

commit 18e6921e8befd8c2cde9ae989a7197fe5bcd1558

Allow negative number in parse_int_range()
This is useful when applying patches and sending a thank you note
following that, which can now be done with `b4 ty -t -1`.

Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

src/b4/__init__.py
@@ -3545,9 +3545,15 @@ def parse_int_range(intrange: str, upper: int) -> Iterator[int]:
     # Remove all whitespace
     intrange = re.sub(r'\s', '', intrange)
     for n in intrange.split(','):
-        if n.isdigit():
-            yield int(n)
-        elif n.find('<') == 0 and len(n) > 1 and n[1:].isdigit():
+        # Allow single numbers to be negative
+        try:
+            i = int(n)
+            if i < 0 and abs(i) <= upper:
+                yield upper + i + 1
+        except ValueError:
+            pass
+
+        if n.find('<') == 0 and len(n) > 1 and n[1:].isdigit():
             yield from range(1, int(n[1:]))
         elif n.find('-') > 0:
             nr = n.split('-')

commit 2ee9fb76c69ddf47c80038df9fe3f0cb4df4bd67

Fix parsing non-negative single numbers with parse_int_range()
Fix a regression when parsing single non-negative numbers. Add a test
for this and other conditions, so we catch this in the future.

Fixes: 18e6921e8b ("Allow negative number in parse_int_range()")
Closes: https://bugzilla.kernel.org/show_bug.cgi?id=218910
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

src/b4/__init__.py
@@ -3551,6 +3551,11 @@ def parse_int_range(intrange: str, upper: int) -> Iterator[int]:
             i = int(n)
             if i < 0 and abs(i) <= upper:
                 yield upper + i + 1
+            elif 0 < i <= upper:
+                yield i
+            else:
+                logger.critical('Unknown range value specified: %s', n)
+                continue
         except ValueError:
             pass
 
@@ -3559,7 +3564,11 @@ def parse_int_range(intrange: str, upper: int) -> Iterator[int]:
         elif n.find('-') > 0:
             nr = n.split('-')
             if nr[0].isdigit() and nr[1].isdigit():
-                yield from range(int(nr[0]), int(nr[1]) + 1)
+                rmax = int(nr[1])
+                if rmax > upper:
+                    logger.debug('Range %s is outside the upper value %s', rmax, upper)
+                    rmax = upper
+                yield from range(int(nr[0]), rmax + 1)
             elif not len(nr[1]) and nr[0].isdigit() and upper:
                 yield from range(int(nr[0]), upper + 1)
         else:

src/tests/test___init__.py
@@ -194,18 +194,34 @@ def test_header_wrapping(sampledir, hval, verify, tr):
 
 @pytest.mark.parametrize('pairs,verify,clean', [
     ([('', 'foo@example.com'), ('Foo Bar', 'bar@example.com')],
-        'foo@example.com, Foo Bar <bar@example.com>', True),
+     'foo@example.com, Foo Bar <bar@example.com>', True),
     ([('', 'foo@example.com'), ('Foo, Bar', 'bar@example.com')],
-        'foo@example.com, "Foo, Bar" <bar@example.com>', True),
+     'foo@example.com, "Foo, Bar" <bar@example.com>', True),
     ([('', 'foo@example.com'), ('Fôo, Bar', 'bar@example.com')],
-        'foo@example.com, "Fôo, Bar" <bar@example.com>', True),
+     'foo@example.com, "Fôo, Bar" <bar@example.com>', True),
     ([('', 'foo@example.com'), ('=?utf-8?q?Qu=C3=BBx_Foo?=', 'quux@example.com')],
      'foo@example.com, Quûx Foo <quux@example.com>', True),
     ([('', 'foo@example.com'), ('=?utf-8?q?Qu=C3=BBx=2C_Foo?=', 'quux@example.com')],
-        'foo@example.com, "Quûx, Foo" <quux@example.com>', True),
+     'foo@example.com, "Quûx, Foo" <quux@example.com>', True),
     ([('', 'foo@example.com'), ('=?utf-8?q?Qu=C3=BBx=2C_Foo?=', 'quux@example.com')],
      'foo@example.com, =?utf-8?q?Qu=C3=BBx=2C_Foo?= <quux@example.com>', False),
 ])
 def test_format_addrs(pairs, verify, clean):
     formatted = b4.format_addrs(pairs, clean)
     assert formatted == verify
+
+
+@pytest.mark.parametrize('intrange,upper,expected', [
+    ('1-3', 5, [1, 2, 3]),
+    ('-1', 5, [5]),
+    ('1,3-5', 5, [1, 3, 4, 5]),
+    ('1', 5, [1]),
+    ('3', 5, [3]),
+    ('5', 5, [5]),
+    ('1,3,4-', 6, [1, 3, 4, 5, 6]),
+    ('1-3,5,-1', 7, [1, 2, 3, 5, 7]),
+    ('-7', 5, []),
+    ('1-8', 3, [1, 2, 3]),
+])
+def test_parse_int_range(intrange, upper, expected):
+    assert list(b4.parse_int_range(intrange, upper)) == expected