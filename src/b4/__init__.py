be43b3606507d46b6d1c6de90594f7dc0203f28e
Konstantin Ryabitsev <konstantin@linuxfoundation.org>
2024-06-14 15:35:59 -0400

    Bump version to 0.15-dev
    Prepare the tree for new features.
    
    Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

--- src/b4/__init__.py
+++ src/b4/__init__.py
@@ -64,7 +64,7 @@ except ModuleNotFoundError:
 # global setting allowing us to turn off networking
 can_network = True
 
-__VERSION__ = '0.14-dev'
+__VERSION__ = '0.15-dev'
 PW_REST_API_VERSION = '1.2'
 
 
