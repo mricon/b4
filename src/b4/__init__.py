mricon/b4

commit 37811c93f50e70f325e45107a9a20ffc69f2f6dc

Forward the smtpserveroption to the local command
Additional arguments to the smtp command are needed depending on the
sendemail identity. Append the additional arguments in smtpserveroptions
to the smtp command.

Signed-off-by: Joel Granados <j.granados@samsung.com>
Link: https://patch.msgid.link/20240723-master-v1-1-52a11f4a96c6@samsung.com
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

src/b4/__init__.py
@@ -3783,6 +3783,9 @@ def get_smtp(dryrun: bool = False) -> Tuple[Union[smtplib.SMTP, smtplib.SMTP_SSL
                 envpair = email.utils.parseaddr(env_sender)
             if envpair[1]:
                 smtp += ['-f', envpair[1]]
+        server_option = sconfig.get('smtpserveroption', '')
+        if server_option:
+            smtp += [server_option]
         logger.debug('sendmail command: %s', ' '.join(smtp))
         return smtp, fromaddr
 

commit 40f6efe297f364e3bec073971e2f29318bb15431

sendemail.smtpServerOption can be specified multiple times
In git-send-email(1):

    The --smtp-server-option option must be repeated for each option you
    want to pass to the server. Likewise, different lines in
    the configuration files must be used for each option.

So this config option must be specified in multivars parameter of
get_config_from_git() and be treated as a list.

The default value in get() will be unreachable, remove it.

Fixes: 37811c93f5 ("Forward the smtpserveroption to the local command")
Cc: Joel Granados <j.granados@samsung.com>
Signed-off-by: Celeste Liu <CoelacanthusHex@gmail.com>
Link: https://patch.msgid.link/20240823-fix-multiple-smtpserveroption-v2-1-e9df83f7b3f1@gmail.com
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

src/b4/__init__.py
@@ -3773,10 +3773,10 @@ def _setup_sendemail_config(cmdargs: argparse.Namespace) -> None:
     # Get the default settings first
     config = get_main_config()
     identity = config.get('sendemail-identity')
-    _basecfg = get_config_from_git(r'sendemail\.[^.]+$')
+    _basecfg = get_config_from_git(r'sendemail\.[^.]+$', multivals=['smtpserveroption'])
     if identity:
         # Use this identity to override what we got from the default one
-        sconfig = get_config_from_git(rf'sendemail\.{identity}\..*', defaults=_basecfg)
+        sconfig = get_config_from_git(rf'sendemail\.{identity}\..*', multivals=['smtpserveroption'], defaults=_basecfg)
         sectname = f'sendemail.{identity}'
         if not len(sconfig):
             raise smtplib.SMTPException('Unable to find %s settings in any applicable git config' % sectname)
@@ -3828,9 +3828,9 @@ def get_smtp(dryrun: bool = False) -> Tuple[Union[smtplib.SMTP, smtplib.SMTP_SSL
                 envpair = email.utils.parseaddr(env_sender)
             if envpair[1]:
                 smtp += ['-f', envpair[1]]
-        server_option = sconfig.get('smtpserveroption', '')
+        server_option = sconfig.get('smtpserveroption')
         if server_option:
-            smtp += [server_option]
+            smtp += server_option
         logger.debug('sendmail command: %s', ' '.join(smtp))
         return smtp, fromaddr
 