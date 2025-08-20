mricon/b4

commit 71916edb52594131affa0402f89722d33d8ab90f

Create patches with --default-prefix
This matches the behaviour of `git format-patch` to ignore diff.noprefix
config (added in git 2.41.0 [1]). Otherwise b4 generated patches will
be missing the top level a/ b/ prefix when diff.noprefix config is set.

[1] https://github.com/git/git/commit/c169af8f7ab521cc47b59f104db78847e324a3cb

Signed-off-by: Matt Johnston <matt@codeconstruct.com.au>
Link: https://patch.msgid.link/20241019-pr-diff-prefix-v1-1-435e6c256e2f@codeconstruct.com.au
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

src/b4/__init__.py
@@ -3469,6 +3469,7 @@ def git_range_to_patches(gitdir: Optional[str], start: str, end: str,
                 '--binary',
                 '--patch-with-stat',
                 '--encoding=utf-8',
+                '--default-prefix',
                 commit,
             ],
             decode=False,

commit 2e5aec1cd7ec324580cde475c3ff6a04a9bf8a50

Add Git version check to prevent crash with '--default-prefix' parameter
The '--default-prefix' parameter was introduced in Git 2.40. Environments
running older versions may crash with errors like:

CRITICAL: Failed to convert range to patches: Could not get a patch out
of 5f8e8d5b2c840f8fc5185c2f90d6d275535a6aea

This change adds a version check to ensure compatibility and prevent such
failures.

Fixes: 71916ed ("Create patches with --default-prefix")
Signed-off-by: Yijie Yang <yijie.yang@oss.qualcomm.com>
Link: https://patch.msgid.link/20250819-ver_check-v3-1-9877d033bb7d@oss.qualcomm.com
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

src/b4/__init__.py
@@ -43,6 +43,7 @@ ConfigDictT = Dict[str, Union[str, List[str], None]]
 from email.message import EmailMessage
 
 from email import charset
+from packaging import version
 
 charset.add_charset('utf-8', None)
 # Policy we use for saving mail locally
@@ -2838,6 +2839,12 @@ def git_run_command(gitdir: Optional[Union[str, Path]], args: List[str], stdin: 
     return _handle(out, err)
 
 
+def git_check_minimal_version(min_version: str) -> bool:
+    ecode, out = git_run_command(None, ["version"])
+    current_version = out.split()[2]
+    return version.parse(current_version) >= version.parse(min_version)
+
+
 def git_credential_fill(gitdir: Optional[str], protocol: str, host: str, username: str) -> Optional[str]:
     stdin = f'protocol={protocol}\nhost={host}\nusername={username}\n'.encode()
     ecode, out = git_run_command(gitdir, args=['credential', 'fill'], stdin=stdin)
@@ -3601,9 +3608,12 @@ def git_range_to_patches(gitdir: Optional[str], start: str, end: str,
             '--binary',
             '--patch-with-stat',
             '--encoding=utf-8',
-            '--default-prefix',
             '--find-renames',
         ]
+
+        if git_check_minimal_version("2.40"):
+            showargs.append("--default-prefix")
+
         smcfg = get_sendemail_config()
         if not get_git_bool(str(smcfg.get('mailmap', 'false'))):
             showargs.append('--no-mailmap')