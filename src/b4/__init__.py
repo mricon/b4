mricon/b4

commit 2e5aec1cd7ec324580cde475c3ff6a04a9bf8a50

Add Git version check to prevent crash with '--default-prefix' parameter
The '--default-prefix' parameter was introduced in Git 2.40. Environments
running older versions may crash with errors like:

CRITICAL: Failed to convert range to patches: Could not get a patch out
of 5f8e8d5b2c840f8fc5185c2f90d6d275535a6aea

This change adds a version check to ensure compatibility and prevent such
failures.

Fixes: 71916ed ("Create patches with --default-prefix")
Signed-off-by: Yijie Yang <yijie.yang@oss.qualcomm.com>
Link: https://patch.msgid.link/20250819-ver_check-v3-1-9877d033bb7d@oss.qualcomm.com
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

src/b4/__init__.py
@@ -43,6 +43,7 @@ ConfigDictT = Dict[str, Union[str, List[str], None]]
 from email.message import EmailMessage
 
 from email import charset
+from packaging import version
 
 charset.add_charset('utf-8', None)
 # Policy we use for saving mail locally
@@ -2838,6 +2839,12 @@ def git_run_command(gitdir: Optional[Union[str, Path]], args: List[str], stdin: 
     return _handle(out, err)
 
 
+def git_check_minimal_version(min_version: str) -> bool:
+    ecode, out = git_run_command(None, ["version"])
+    current_version = out.split()[2]
+    return version.parse(current_version) >= version.parse(min_version)
+
+
 def git_credential_fill(gitdir: Optional[str], protocol: str, host: str, username: str) -> Optional[str]:
     stdin = f'protocol={protocol}\nhost={host}\nusername={username}\n'.encode()
     ecode, out = git_run_command(gitdir, args=['credential', 'fill'], stdin=stdin)
@@ -3601,9 +3608,12 @@ def git_range_to_patches(gitdir: Optional[str], start: str, end: str,
             '--binary',
             '--patch-with-stat',
             '--encoding=utf-8',
-            '--default-prefix',
             '--find-renames',
         ]
+
+        if git_check_minimal_version("2.40"):
+            showargs.append("--default-prefix")
+
         smcfg = get_sendemail_config()
         if not get_git_bool(str(smcfg.get('mailmap', 'false'))):
             showargs.append('--no-mailmap')

commit 5618363ab0142358a7a6a45e381fb6b7c10fe9b5

Don't parse nonstandard git-version suffix
In snapshot builds of git, git-version yields a nonstandard version
format like 2.51.0.178.g2462961280. packaging.version.parse() errors out
on this with an InvalidVersion exception.

Since we only need to compare major and minor version, and
packaging.version is intended for a different version scheme, resort to
comparing the two numbers manually.

Fixes: 2e5aec1cd7ec ("Add Git version check to prevent crash with '--default-prefix' parameter")
Signed-off-by: Jonas Rebmann <jre@pengutronix.de>
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

src/b4/__init__.py
@@ -43,7 +43,6 @@ ConfigDictT = Dict[str, Union[str, List[str], None]]
 from email.message import EmailMessage
 
 from email import charset
-from packaging import version
 
 charset.add_charset('utf-8', None)
 # Policy we use for saving mail locally
@@ -2841,8 +2840,9 @@ def git_run_command(gitdir: Optional[Union[str, Path]], args: List[str], stdin: 
 
 def git_check_minimal_version(min_version: str) -> bool:
     ecode, out = git_run_command(None, ["version"])
-    current_version = out.split()[2]
-    return version.parse(current_version) >= version.parse(min_version)
+    current_version = re.sub(r"git version (\d+\.\d+)\..*", r"\1", out)
+    return tuple(map(int, current_version.split(".")[:2])) >= tuple(map(int, min_version.split(".")[:2]))
+
 
 
 def git_credential_fill(gitdir: Optional[str], protocol: str, host: str, username: str) -> Optional[str]: