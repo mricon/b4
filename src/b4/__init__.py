mricon/b4

commit 7ccea87a5f6898e6ef398b44cd2c2ed4463515e3

b4: allow using xoauth2/bearer token to authenticate to SMTP servers
Allow using XOAUTH2 as an authentication protocol and assume that when
XOAUTH2 is specified the password is actually a bearer token (typically
not stored in the config but rather returned via "git credentials".

Recognize "oauth", "oauth2" as aliases for "xoauth2".

Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
Link: https://patch.msgid.link/20260306162020.54683-1-dtor@chromium.org
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

src/b4/__init__.py
@@ -4337,8 +4337,8 @@ def get_smtp(dryrun: bool = False) -> Tuple[Union[smtplib.SMTP, smtplib.SMTP_SSL
 
         # If we got to this point, we should do authentication,
         # unless smtpauth is set to a special "none" value
-        smtpauth = str(sconfig.get('smtpauth', ''))
-        if smtpauth.lower() == 'none':
+        smtpauth = str(sconfig.get('smtpauth', '')).lower()
+        if smtpauth == 'none':
             return smtp, fromaddr
 
         auser = str(sconfig.get('smtpuser', ''))
@@ -4356,7 +4356,11 @@ def get_smtp(dryrun: bool = False) -> Tuple[Union[smtplib.SMTP, smtplib.SMTP_SSL
                 raise smtplib.SMTPException('No password specified for connecting to %s', server)
         if auser and apass:
             # Let any exceptions bubble up
-            smtp.login(auser, apass)
+            if smtpauth in ('oauth', 'oauth2', 'xoauth2'):
+                auth_str = f'user={auser}\x01auth=Bearer {apass}\x01\x01'
+                smtp.auth('XOAUTH2', lambda: auth_str)
+            else:
+                smtp.login(auser, apass)
     else:
         # We assume you know what you're doing if you don't need encryption
         smtp = smtplib.SMTP(server, port)

commit 3f630f038841cce2f09cdc0286f3ad3ec430157f

Fix XOAUTH2 auth callback to accept server challenge argument
smtplib.SMTP.auth() calls the authentication callback with a
positional bytes argument when the server responds with a 334
challenge code. The current lambda accepts no arguments, causing
a TypeError that masks the actual authentication error from the
server.

Change the lambda to accept an optional argument, returning the
auth string on the initial call and an empty string on any
subsequent 334 challenge, allowing the server to communicate its
genuine error code.

Fixes: 7ccea87a5f68 ("b4: allow using xoauth2/bearer token to authenticate to SMTP servers")
Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>
Assisted-by: claude-opus-4-6

src/b4/__init__.py
@@ -4374,7 +4374,7 @@ def get_smtp(dryrun: bool = False) -> Tuple[Union[smtplib.SMTP, smtplib.SMTP_SSL
             # Let any exceptions bubble up
             if smtpauth in ('oauth', 'oauth2', 'xoauth2'):
                 auth_str = f'user={auser}\x01auth=Bearer {apass}\x01\x01'
-                smtp.auth('XOAUTH2', lambda: auth_str)
+                smtp.auth('XOAUTH2', lambda x=None: auth_str if x is None else '')
             else:
                 smtp.login(auser, apass)
     else: