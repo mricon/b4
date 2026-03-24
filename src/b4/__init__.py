de77658b62f0a7c704232c97caab50556b832bfb
Konstantin Ryabitsev <konstantin@linuxfoundation.org>
2026-03-24 10:07:57 -0400

    Bump version to 0.15.1
    Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

--- src/b4/__init__.py
+++ src/b4/__init__.py
@@ -69,7 +69,7 @@ except ModuleNotFoundError:
 # global setting allowing us to turn off networking
 can_network = True
 
-__VERSION__ = '0.15.0'
+__VERSION__ = '0.15.1'
 PW_REST_API_VERSION = '1.2'
 
 
